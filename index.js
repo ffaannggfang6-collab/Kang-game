3f61242e51f60f49dab8e5bbd21948b4e1e063a6
ffaannggfang6-collab <ffaannggfang6@gmail.com>
2026-01-20 05:57:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,39 +9,67 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
-/* ===== เสิร์ฟ client ===== */
+/* ===== serve client ===== */
 app.use(express.static(path.join(__dirname, 'client')));
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
+/* ===== GAME DATA ===== */
+const ROOMS = {
+  10: { price: 10, fee: 1, players: [] },
+  20: { price: 20, fee: 1, players: [] },
+  50: { price: 50, fee: 1, players: [] },
+  70: { price: 70, fee: 2, players: [] },
+  100:{ price:100, fee: 2, players: [] }
+};
+
+const users = {};
+
 /* ===== SOCKET ===== */
 io.on('connection', (socket) => {
-  console.log('เชื่อมต่อ:', socket.id);
 
   socket.on('login', (username) => {
-    console.log('login:', username);
-    socket.emit('update_data', {
-      credit: 1000
+    users[socket.id] = { name: username, credit: 1000 };
+    socket.emit('login_success', {
+      user: users[socket.id],
+      rooms: getRoomInfo()
     });
   });
 
-  socket.on('join_room', (price) => {
-    socket.join('room_' + price);
-    console.log('เข้าห้องราคา', price);
-  });
+  socket.on('join_room', (roomPrice) => {
+    const room = ROOMS[roomPrice];
+    if (!room) return;
 
-  socket.on('player_action', (data) => {
-    console.log('action:', data);
+    if (room.players.length >= 5) {
+      socket.emit('room_full');
+      return;
+    }
+
+    room.players.push(socket.id);
+    socket.join('room_' + roomPrice);
+
+    io.emit('room_update', getRoomInfo());
   });
 
   socket.on('disconnect', () => {
-    console.log('ออก:', socket.id);
+    Object.values(ROOMS).forEach(room => {
+      room.players = room.players.filter(id => id !== socket.id);
+    });
+    delete users[socket.id];
+    io.emit('room_update', getRoomInfo());
   });
 });
 
-/* ===== START ===== */
+/* ===== helper ===== */
+function getRoomInfo() {
+  const info = {};
+  for (let key in ROOMS) {
+    info[key] = ROOMS[key].players.length;
+  }
+  return info;
+}
+
 server.listen(PORT, () => {
   console.log('Kang game server running on port', PORT);
 });
