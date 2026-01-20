cfdba3136997780f82bd5709f32bb3b3eae7778b
ffaannggfang6-collab <ffaannggfang6@gmail.com>
2026-01-20 08:29:23 +0700

    Delete game/deck.js

--- game/deck.js
+++
@@ -1,4 +0,0 @@
-// game/deck.js
-const suits=['C','D','H','S']; const ranks=[1,2,3,4,5,6,7,8,9,10,10,10,10];
-exports.createDeck=()=>shuffle(suits.flatMap(s=>ranks.map(v=>({s,v}))));
-const shuffle=d=>d.sort(()=>Math.random()-0.5);
